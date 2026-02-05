da1517170058406da020de4fde288dd50dee788a
Agustin <agustindelgadohaupt@gmail.com>
2026-02-05 19:41:51 -0300

    Refactor reservar function and implement auto carousel
    Updated the reservar function and added auto slide functionality.

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function reservar() {
 
-    let servicio = document.getElementById("servicio").value;
-  
-    let telefono = "5491123456789"; // CAMBIAR
-  
-    let mensaje = `Hola! Quiero reservar turno para ${servicio}`;
-  
-    let url = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
-  
-    window.open(url, "_blank");
-  }
+  let servicio = document.getElementById("servicio").value;
+
+  let telefono = "5491123456789"; // CAMBIAR POR EL REAL
+
+  let mensaje = `Hola! Quiero reservar turno para ${servicio}`;
+
+  let url = `https://wa.me/${telefono}?text=${encodeURIComponent(mensaje)}`;
+
+  window.open(url, "_blank");
+}
+
+/* CARROUSEL */
+
 let index = 0;
 
 function mover(direccion) {
@@ -25,4 +28,10 @@ function mover(direccion) {
   slides.style.transform = `translateX(-${index * 100}%)`;
 }
 
-  
+/* AUTO */
+
+setInterval(() => {
+  mover(1);
+}, 3000);
+
+
